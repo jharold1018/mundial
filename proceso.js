243d3c5b35dff3d357788c4077e94939e041f425
jharold1018 <jharol1018@gmail.com>
2023-06-29 09:53:39 -0500

    first commit

---
+++ proceso.js
@@ -0,0 +1,45 @@
+function tico() {
+    document.getElementById('estadio1').innerHTML= ""
+    +"<div><ul><li>Estadio Arena Castelão</li></ul></div>"
+    +"<div><ul><li>O local foi inaugurado em 11 de novembro de 1973 e tem capacidade máxima para 65.000 espectadores sentados após sua última reforma em 2013.</li></ul></div> ";
+}
+function tica() {
+    document.getElementById('estadio2').innerHTML= ""
+    +"<div><ul><li>Estadio Beira-Rio</li></ul></div><br>"
+    +"Também conhecido como José Pinheiro Borda é um estádio de futebol brasileiro localizado na cidade de Porto Alegre, na costa do Rio Guaíba, estado do Rio Grande do Sul. O time ao qual pertence é o Internacional";
+}
+function ticy() {
+    document.getElementById('estadio3').innerHTML= ""
+    +"<div><li><ul>Estadio Mineirão</li></ul></div>"
+    +"Inaugurado em 1965, o Mineirão tem capacidade para 60 mil espectadores. Sofreu uma redução significativa em sua capacidade de se enquadrar nas regras da FIFA para estádios de futebol.";
+}
+function ticu() {
+    document.getElementById('estadio4').innerHTML= ""
+    +"<div><li><ul>Estadio de Maracaná</li></ul></div>"
+    +"É o maior estádio do Brasil e foi por muito tempo o maior do mundo. Sua construção começou no início de 1949 como parte dos esforços locais para obter a sede da Copa do Mundo de Futebol.";
+}
+function ticb() {
+    document.getElementById('estadio5').innerHTML= ""
+    +"<div><li><ul>Estadio de brasilia </li></ul></div>"
+    +" é um estádio multiuso localizado em Brasília, no Distrito Federal, no Brasil. Foi inaugurado em 1974 e após a última remodelação, em 2013, tem capacidade para 72.800 espectadores sentados.";
+}
+function agenda() {
+   document.getElementById('agenda').innerHTML= ""
+   +"<div><li><ul>agenda de eventos</li></ul></div>"
+   +"<p>Para esta grande inauguração teremos de mãos dadas Brasil e Croácia, que serão disputados na tarde deste 12 de junho.</p>";
+}
+function lema() {
+    document.getElementById('lema').innerHTML= ""
+    +"<div><ul><li>lema oficial da copa do mundo</li></ul></div>"
+    +"<p>O lema oficial escolhido para a Copa do Mundo que o Brasil organizará em 2014 é 'Juntos num só ritmo'('Todos al mismo ritmo').</p>";
+}
+function patrocinador1() {
+    document.getElementById('patrocinador1').innerHTML= ""
+    +"<div><li><ul>coca-cola</li></ul></div>"
+    +"<p>A Coca-Cola sempre foi um dos principais e mais importantes patrocinadores de diversos eventos no mundo</p>";
+}
+function patrocinador2() {
+    document.getElementById('patrocinador2').innerHTML= ""
+    +"<div><li><ul>adidas</li></ul></div>"
+    +"<p>adidas, a marca de roupas esportivas, deu seu patrocínio apresentando novos modelos de calçados esportivos e diferentes implementos adicionais</p>";
+}
